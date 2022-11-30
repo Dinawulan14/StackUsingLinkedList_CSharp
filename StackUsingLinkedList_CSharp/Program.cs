50fb073348431b9d0ef107bd68465ec2699c403b
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-11-30 15:43:19 +0700

    membuat public int info

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -8,7 +8,7 @@ namespace StackUsingLinkedList_CSharp
 {
     class Node
     {
-        public int info;
+        public int info; 
         public Node next;
         public Node(int i, Node n)
         {
