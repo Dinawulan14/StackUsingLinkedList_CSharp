e6d24c9b6c441a24f3e420eab70956fbad3a0056
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-11-30 15:59:07 +0700

    Menambahkan method public void display

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -48,5 +48,21 @@ namespace StackUsingLinkedList_CSharp
             Console.WriteLine("\nThe popped element is: " + top.info); 
             top = top.next; //Make top point to the next node in equence 
         }
+
+        public void display()
+        {
+            Node tmp;
+            if (empty()) //if stack is empty
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                //Traverse the list from beginning till end
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
     }
 } 
