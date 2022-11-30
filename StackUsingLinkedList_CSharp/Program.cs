4b1ad9037bf9e113ce0bfeb6025624510dbef049
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-11-30 15:43:35 +0700

    Membuat public Node next

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -9,7 +9,7 @@ namespace StackUsingLinkedList_CSharp
     class Node
     {
         public int info; 
-        public Node next;
+        public Node next; 
         public Node(int i, Node n)
         {
             info = i;
