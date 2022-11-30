28e6eae148aa36458810777fa45d17fc1c989693
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-11-30 15:44:35 +0700

    Membuat class Stacks

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -8,12 +8,21 @@ namespace StackUsingLinkedList_CSharp
 {
     class Node
     {
-        public int info; 
-        public Node next; 
+        public int info;
+        public Node next;
         public Node(int i, Node n)
         {
             info = i;
             next = n;
+        }
+    }
+    class Stacks
+    {
+        Node top;
+
+        public Stacks()
+        {
+            top = null;
         }
     }
 }
